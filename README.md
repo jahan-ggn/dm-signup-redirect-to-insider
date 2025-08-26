40a6e67ded3ba5fa0c35f88d0d8a714c22df4631
Jahan Gagan <jahanggn9@gmail.com>
2025-08-26 17:23:44 +0530

    feat: add theme initializer to redirect signup to external page when Discourse Connect is enabled

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# dm-signup-redirect-to-insider
+# DM Signup Redirect To Insider
 
 **Theme Summary**
 
-For more information, please see: **url to meta topic**
+Redirects users to the Daily Maverick Insider page when they click the Sign Up button, instead of showing the default Discourse signup form. This customization is active only when Discourse Connect (SSO) is enabled.
---
+++ javascripts/discourse/api-initializers/dm-signup-redirect-to-insider.js
@@ -0,0 +1,25 @@
+import { getOwner } from "@ember/owner";
+import { withPluginApi } from "discourse/lib/plugin-api";
+
+export default {
+  name: "dm-signup-redirect-to-insider",
+  initialize() {
+    withPluginApi((api) => {
+      api.modifyClass("route:signup", {
+        beforeModel(transition) {
+          const siteSettings = getOwner(this).lookup("service:site-settings");
+          const enable_discourse_connect =
+            siteSettings.enable_discourse_connect;
+
+          if (enable_discourse_connect) {
+            transition.abort();
+            window.open(settings.sign_up_redirect_url, "_blank");
+            return;
+          }
+
+          this._super(...arguments);
+        },
+      });
+    });
+  },
+};
